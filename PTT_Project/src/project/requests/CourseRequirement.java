7be97dc8a18b7dd8ca672a51d5d0c65f8efde240
Tony Lydon <lydont4@gmail.com>
2020-02-27 19:08:09 +0000

    Updated TrainingRequirement and CourseRequirement.

--- PTT_Project/src/project/requests/CourseRequirement.java
+++ PTT_Project/src/project/requests/CourseRequirement.java
@@ -1,17 +1,59 @@
 package project.requests;
 
 import project.requests.course.Course;
+import project.requests.course.Qualification;
 
 public class CourseRequirement 
 {
 
 	private Course course;
+	private int contactHours;
+	private String typeOfContact;
+	private int numberOfStaff;
+	private Qualification qualification;
+	private int classSize;
 	
-	// Attribs.
-	
-	public CourseRequirement(final Course course)
+	public CourseRequirement(final Course course, final int contactHours, final String typeOfContact,
+							 final int numberOfStaff, final Qualification qualification, final int classSize)
 	{
 		this.course = course;
+		this.contactHours = contactHours;
+		this.typeOfContact = typeOfContact;
+		this.numberOfStaff = numberOfStaff;
+		this.qualification = qualification;
+		this.classSize = classSize;
+	}
+	
+	public int getContactHours() {
+		return contactHours;
+	}
+
+	public void setContactHours(int contactHours) {
+		this.contactHours = contactHours;
+	}
+
+	public String getTypeOfContact() {
+		return typeOfContact;
+	}
+
+	public void setTypeOfContact(String typeOfContact) {
+		this.typeOfContact = typeOfContact;
+	}
+
+	public int getNumberOfStaff() {
+		return numberOfStaff;
+	}
+
+	public void setNumberOfStaff(int numberOfStaff) {
+		this.numberOfStaff = numberOfStaff;
+	}
+
+	public int getClassSize() {
+		return classSize;
+	}
+
+	public void setClassSize(int classSize) {
+		this.classSize = classSize;
 	}
 	
 	public Course getCourse()
@@ -22,6 +64,14 @@ public class CourseRequirement
 	public void setCourse(final Course course)
 	{
 		this.course = course;
+	}
+
+	public Qualification getSkill() {
+		return qualification;
+	}
+
+	public void setSkill(Qualification skill) {
+		this.qualification = skill;
 	}
 
 }
--- PTT_Project/src/project/requests/TrainingRequirement.java
+++ PTT_Project/src/project/requests/TrainingRequirement.java
@@ -1,6 +1,28 @@
 package project.requests;
 
+import project.requests.course.Qualification;
+import project.requests.course.Teacher;
+
 public class TrainingRequirement 
 {
+	private Qualification qualification;
+	private Teacher teacher;
 	
+	public TrainingRequirement (final Qualification qualification, final Teacher teacher) {
+		this.qualification = qualification;
+		this.teacher = teacher;
+	}
+	
+	public Qualification getQualification() {
+		return qualification;
+	}
+	public void setQualification(Qualification qualification) {
+		this.qualification = qualification;
+	}
+	public Teacher getTeacher() {
+		return teacher;
+	}
+	public void setTeacher(Teacher teacher) {
+		this.teacher = teacher;
+	}
 }
