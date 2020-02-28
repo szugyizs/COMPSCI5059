34e0471f9681dfc9cde66d8eedd776a7167bc2c5
Belinda <belinda.kneubuehler@gmail.com>
2020-02-28 10:14:47 +0000

    added Teacher constructors (various ways) and basic getters/setters

--- PTT_Project/src/project/requests/course/Teacher.java
+++ PTT_Project/src/project/requests/course/Teacher.java
@@ -2,5 +2,49 @@ package project.requests.course;
 
 public class Teacher
 {
+	private String name;
+	private Qualification qualification;
+	
+	protected Teacher(String name)
+	{
+		this.name = name;
+		this.qualification = new Qualification("None", 0); 
+		//TODO: how to handle teachers with no skills? empty string or "None"?
+		// should they be allowed at all? Should the skill (name) be allowed to change?
+	}
+	
+	protected Teacher(String name, Qualification qualification)
+	{
+		this.name = name;
+		this.qualification = qualification;
+	}
+
+	protected Teacher(String name, String skill, int level)
+	{
+		this.name = name;
+		this.qualification = new Qualification(skill, level);
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+
+	public Qualification getQualification()
+	{
+		return qualification;
+	}
+
+	public void setQualification(Qualification qualification)
+	{
+		this.qualification = qualification;
+	}
+
+	//TODO: needed
+	public void setQualification(String skill, int level)
+	{
+		this.qualification.setSkill(skill, level);
+	}
+	
 	
 }
