820d17c04d36d4f82c5250ea54c51fd928307da6
Tony Lydon <lydont4@gmail.com>
2020-02-29 20:16:17 +0000

    CourseRequirements updated to include ContactType enum

--- PTT_Project/src/project/requests/CourseRequirement.java
+++ PTT_Project/src/project/requests/CourseRequirement.java
@@ -7,180 +7,83 @@ import project.requests.course.Qualifications;
 public class CourseRequirement 
 {
 	
+	private ContactType contactType;
+	private int numberOfStudents;
+	private int numberOfStaff;
+	private int contactHours;
+	private Qualifications requiredStaffQualifications;
+
+	
+	public CourseRequirement(final Course course, final ContactType contactType, final int contactHours, final String typeOfContact,
+							 final int numberOfStaff, final Qualifications requiredStaffQualifications, final int numberOfStudents)
+	{
+		this.course = course;
+		this.contactType = contactType;
+		this.contactHours = contactHours;
+		this.numberOfStaff = numberOfStaff;
+		this.requiredStaffQualifications = requiredStaffQualifications;
+		this.numberOfStudents = numberOfStudents;
+	}
 	
 
 	private Course course;
-	
-	private ContactType contactType;
-	
-	private short numLecturers;
-	private short numTutors;
-	private short numDemonstrators;
-	private short numStudents;
-	
-	private short lectureContactHours;
-	private short tutorialContactHours;
-	private short labContactHours; 
-	
-	private Qualifications requiredStaffQualifications;
-	
-	/**
-	 * Constructor is used to explicitly initialise the course requirements with the required parameters.
-	 * 
-	 * @param course Instance for which the requirements are being requested. 
-	 * @param numLecturers Represents the number of lecturers required for the said Course.
-	 * @param numTutors Represents the number of tutors required for tutorials.
-	 * @param numDemonstrators Represents the number of lab demonstrators required for the said Course.
-	 * @param numStudents Maximum number of students taking the Course.
-	 * @param lectureContactHours Number of lecture contact hours for the said Course.
-	 * @param tutorialContactHours Number of tutorials for the said Course instance.
-	 * @param labContactHours Number of lab contact hours for the said Course instance.
-	 * @param requiredStaffQualifications The qualifications required by lecturers, tutors, and demonstrators.
-	 */
-	
-	
-	
-	
-	private CourseRequirement(final Course course, final short numLecturers, final short numTutors, 
-			final short numDemonstrators, final short numStudents, final short lectureContactHours, 
-			final short tutorialContactHours, final short labContactHours, final Qualifications requiredStaffQualifications)
-	{
-		this.course = course;
-		
-		this.numLecturers = numLecturers;
-		this.numTutors = numTutors;
-		this.numDemonstrators = numDemonstrators;
-		this.numStudents = numStudents;
-		
-		this.lectureContactHours = lectureContactHours;
-		this.tutorialContactHours = tutorialContactHours;
-		this.labContactHours = labContactHours;
-		
-		this.requiredStaffQualifications = requiredStaffQualifications;
-	}
-	
-	/**
-	 * @return The Course for which the requirements are being made.
-	 */
-	public Course getCourse() 
-	{
+	public Course getCourse() {
 		return course;
 	}
 
-	/**
-	 * @param course Course instance for which the requirements are being made.
-	 */
-	public void setCourse(final Course course) 
-	{
+
+	public void setCourse(Course course) {
 		this.course = course;
 	}
-	
-	/**
-	 * @return The number of required lecturers for the Course instance.
-	 */
-	public short getNumLecturers() 
-	{
-		return numLecturers;
+
+
+	public ContactType getContactType() {
+		return contactType;
 	}
 
-	/**
-	 * @param numLecturers The number of lecturers required for the Course instance.
-	 */
-	public void setNumLecturers(final short numLecturers)
-	{
-		this.numLecturers = numLecturers;
+
+	public void setContactType(ContactType contactType) {
+		this.contactType = contactType;
 	}
 
-	/**
-	 * @return The number of tutors required to take the tutorials for the Course.
-	 */
-	public short getNumTutors() 
-	{
-		return numTutors;
+
+	public int getNumberOfStudents() {
+		return numberOfStudents;
 	}
 
-	/**
-	 * @param numTutors The number of tutors required to take the tutorials for the course.
-	 */
-	public void setNumTutors(final short numTutors)
-	{
-		this.numTutors = numTutors;
+
+	public void setNumberOfStudents(int numberOfStudents) {
+		this.numberOfStudents = numberOfStudents;
 	}
 
-	public short getNumDemonstrators() 
-	{
-		return numDemonstrators;
+
+	public int getNumberOfStaff() {
+		return numberOfStaff;
 	}
 
-	public void setNumDemonstrators(final short numDemonstrators)
-	{
-		this.numDemonstrators = numDemonstrators;
+
+	public void setNumberOfStaff(int numberOfStaff) {
+		this.numberOfStaff = numberOfStaff;
 	}
 
-	public short getNumStudents()
-	{
-		return numStudents;
+
+	public int getContactHours() {
+		return contactHours;
 	}
 
-	public void setNumStudents(final short numStudents) 
-	{
-		this.numStudents = numStudents;
-	}
-	
-	public short getLectureContactHours() 
-	{
-		return lectureContactHours;
+
+	public void setContactHours(int contactHours) {
+		this.contactHours = contactHours;
 	}
 
-	public void setLectureContactHours(final short lectureContactHours) 
-	{
-		this.lectureContactHours = lectureContactHours;
-	}
 
-	public short getTutorialContactHours()
-	{
-		return tutorialContactHours;
-	}
-
-	public void setTutorialContactHours(final short tutorialContactHours) 
-	{
-		this.tutorialContactHours = tutorialContactHours;
-	}
-
-	public short getLabContactHours()
-	{
-		return labContactHours;
-	}
-
-	public void setLabContactHours(final short labContactHours) 
-	{
-		this.labContactHours = labContactHours;
-	}
-
-	public Qualifications getRequiredStaffQualifications()
-	{
+	public Qualifications getRequiredStaffQualifications() {
 		return requiredStaffQualifications;
 	}
 
-	public void setRequiredStaffQualifications(final Qualifications requiredStaffQualifications)
-	{
+
+	public void setRequiredStaffQualifications(Qualifications requiredStaffQualifications) {
 		this.requiredStaffQualifications = requiredStaffQualifications;
-	}
-	
-	@Override
-	public String toString()
-	{
-		return String.format("Course: %s, Lecturers: %d, Tutors: %d, Demonstrators: %d, Students: %d," 
-				+ " Lecture contact hours: %d, Tutorial contact hours: %d, Lab contact hours: %d, Qualifications: %s",
-				course.getName(),
-				numLecturers, 
-				numTutors, 
-				numDemonstrators,
-				numStudents,
-				lectureContactHours, 
-				tutorialContactHours, 
-				labContactHours, 
-				requiredStaffQualifications.toString());
 	}
 
 }
