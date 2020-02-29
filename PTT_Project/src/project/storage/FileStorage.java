33a292fe4d8099fc3354b4f45765d6f99cc83877
szugyizs <zsuzsanna.szugyi@gmail.com>
2020-02-29 19:48:37 +0000

    Added the JUnit and JSON-simple JARs in folder lib in root.

--- PTT_Project/src/project/storage/FileStorage.java
+++ PTT_Project/src/project/storage/FileStorage.java
@@ -1,5 +1,11 @@
 package project.storage;
 
+import java.io.FileWriter;
+import java.io.IOException; 
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 public class FileStorage extends Storage
 {
 	
