4e651cfe6114596719a7ba91b568257ce159aa11
dans-acc <danv123456789@gmail.com>
2020-03-01 21:22:29 +0000

    Prototype storage interface.

--- PTT_Project/src/project/storage/Storage.java
+++ PTT_Project/src/project/storage/Storage.java
@@ -1,9 +1,19 @@
 package project.storage;
 
 import project.storage.lists.ListOfRequests;
+import project.storage.lists.ListOfTeachers;
 
 public abstract class Storage 
 {
 	
+	public ListOfTeachers getListOfTeachers()
+	{
+		return null;
+	}
+	
+	public ListOfCourses getListOfCourses()
+	{
+		return null;
+	}
 	
 }
